1d77898d48228f6c0cf8d21db705b0234e8dbdf8
brahim-dev <59501110+brahim-dev@users.noreply.github.com>
2020-04-17 01:34:40 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class database{
+    
+ private   $bd;
+  function  __construct(){
+        try {
+            $this->bd = new PDO("mysql:host=localhost;dbname=ecomerce", "root", "");
+            // set the PDO error mode to exception
+            //$bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+            }
+        catch(PDOException $e)
+            {
+            echo "Connection failed: " . $e->getMessage();
+            }
+    }
+    public function getDb() {
+        if ($this->bd instanceof PDO) {
+             return $this->bd;
+        }
+  }
+
+}
+
+?>
